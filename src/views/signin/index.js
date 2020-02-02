07ae1dd82a5b43d676e7595f316061924277b317
caleb ewere ebie <caleb42.codes@gmail.com>
2020-02-02 11:07:52 +0100

    complete auth

---
+++ src/views/signin/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { LogIn } from '../../utils/anotherutil/firebase/auth';
+
+const SignIn = () => {
+  const [ phone, getPhone ] = React.useState('');
+  const [ password, getPassword ] = React.useState('');
+
+  const SignInUser = () => {
+    LogIn({ phone, password })
+    .then(resp=>console.log(resp))
+    .catch(err=>console.log(err))
+  }
+
+  return (
+    <div className="App">
+      <div className="bg-white w-1/4 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="phoneNumber"
+          >
+            phone Number
+          </label>
+          <input
+            onChange={(e)=>getPhone(e.currentTarget.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="phoneNumber"
+            type="text"
+            placeholder="phone number"
+          />
+        </div>
+        <div id='recaptcha-container' className='m-5'>
+
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="verification"
+          >
+            password
+          </label>
+          <input
+            onChange={(e)=>getPassword(e.currentTarget.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="password"
+            type="text"
+            placeholder="password"
+          />
+        </div>
+        <button onClick={SignInUser} className='btn bg-teal-400 border-sm'>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
--- src/views/signup/index.js
+++ src/views/signup/index.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import { initializeCaptcha, VerifyPhoneNumber, SignUp } from '../../firebase/auth';
+import { InitializeCaptcha, UserExist, SetPassword, SignOut, VerifyPhoneNumber, SignUp } from '../../utils/anotherutil/firebase/auth';
 
 const Signup = () => {
   const [ phone, getPhone ] = React.useState('');
   const [ verification, getVerification ] = React.useState('');
+  const [ password, getPassword ] = React.useState('');
+  const [ verify, showVerify ] = React.useState(false);
+  const [ verified, getVerified ] = React.useState(false);
 
-  React.useEffect(()=>{
-    initializeCaptcha('recaptcha-container')
-  }, [])
-
-  const captcha = ()=>{
-    VerifyPhoneNumber(phone)
+  const captcha = async ()=>{
+    try{
+      const userExist = await UserExist(phone);
+      if(userExist.status === 'success')
+      return alert(userExist.message)
+      InitializeCaptcha('recaptcha-container').then(resp => {
+        VerifyPhoneNumber(phone)
+        .then(resp=>{
+          console.log(resp)
+          showVerify(true)
+        })
+        .catch(err=>console.log(err))
+      })
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   const verifyUser = () => {
     SignUp(verification)
+    .then(resp=>{
+      console.log(resp)
+      getVerified(true);
+    })
+    .catch(err=>console.log(err))
+  }
+
+  const SignOutUser = () => {
+    SignOut()
+    .then(resp=>console.log('user signed out'))
+    .catch(err=>console.log('user signed error'))
+  }
+
+  const SavePassword = () => {
+    SetPassword(password)
+    .then(resp=>console.log(resp))
+    .catch(err=>console.log(err))
   }
 
   return (
@@ -37,29 +67,50 @@ const Signup = () => {
         </div>
         <div id='recaptcha-container' className='m-5'>
 
+        </div >
+          <button onClick={captcha} className='btn bg-teal-400 border-sm'>
+            {!verify ? 'Send OTP' : 'Resend OTP'}
+          </button>
+          <div className={`mb-4 ${verify ? 'inline-block': 'hidden'}`}>
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="verification"
+            >
+              Code
+            </label>
+            <input
+              onChange={(e)=>getVerification(e.currentTarget.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="verifyphone"
+              type="text"
+              placeholder="verification number"
+            />
+          </div>
+          <button onClick={verifyUser}  className={`btn bg-teal-400 border-sm ${verification ? 'inline-block': 'hidden'}`}>
+            Sign up
+          </button>
+          <div className={`mb-4 ${verified ? 'inline-block': 'hidden'}`}>
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="verification"
+            >
+              password
+            </label>
+            <input
+              onChange={(e)=>getPassword(e.currentTarget.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="password"
+              type="text"
+              placeholder="password"
+            />
+          </div>
+          <button onClick={SavePassword} className={`btn bg-teal-400 border-sm ${verified ? 'inline-block': 'hidden'}`}>
+            Save password
+          </button>
+          <button onClick={SignOutUser} className={`btn bg-teal-400 border-sm ${verified ? 'inline-block': 'hidden'}`}>
+            Sign out
+          </button>
         </div>
-        <button onClick={captcha} className='btn bg-teal-400 border-sm'>
-          Verify
-        </button>
-        <div className="mb-4">
-          <label
-            className="block text-grey-darker text-sm font-bold mb-2"
-            htmlFor="verification"
-          >
-            Code
-          </label>
-          <input
-            onChange={(e)=>getVerification(e.currentTarget.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-            id="verifyphone"
-            type="text"
-            placeholder="verification number"
-          />
-        </div>
-        <button onClick={verifyUser} className='btn bg-teal-400 border-sm'>
-          Sign up
-        </button>
-      </div>
     </div>
   );
 }
